101e305e27a3170fe555f4e4a5b7d67d251e3452
Hugo Jr. <00161417@uca.edu.sv>
2018-05-28 17:13:31 -0600

    Cambio del metodo DecimalABinario a Binario y commit pasado se elimono el boton " btn " y sus funciones

---
+++ BuilderDesingPattern/src/concesionario/workes/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concesionario.workes;
+
+/**
+ *
+ * @author Hugo Jr. <hugo at 00161417@uca.edu.sv>
+ */
+public class NewClass {
+    
+}
